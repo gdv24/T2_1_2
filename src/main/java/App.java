7fb5f55b97540489d3c86bf52900a11845ea4882
nikitanesterenkos@gmail.com <piramida741147>
2020-07-03 12:55:27 +0300

    fix log

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,21 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean bHelloWorld = bean == bean2;
+        System.out.println(bHelloWorld);
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+
+        boolean bCat = beanCat == beanCat2;
+        System.out.println(bCat);
+
         System.out.println(bean.getMessage());
+        System.out.println(beanCat.getMessage());
     }
 }
